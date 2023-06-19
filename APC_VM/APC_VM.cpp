8aead91e27291dbb9e8bea28e7a3d41bcd937101
Huoji's <1296564236@qq.com>
2023-06-19 21:37:50 +0800

    add readme

--- APC_VM/APC_VM.cpp
+++ APC_VM/APC_VM.cpp
@@ -25,40 +25,9 @@ struct hpet_acpi_data {
     uint8_t flags;
 };
 static const auto FirmwareTableProviderSignature = 'ACPI';
-
-auto getHPETData() -> std::pair<bool, std::shared_ptr<hpet_acpi_data>> {
-    static const auto HPETSignature = 'TEPH';
-    const auto tableBufferSize = GetSystemFirmwareTable(
-        FirmwareTableProviderSignature, HPETSignature, NULL, NULL);
-    hpet_acpi_data hpetData;
-    bool isSuccess = false;
-    if (tableBufferSize == 0) {
-        return {isSuccess, nullptr};
-    }
-    const auto tableBuffer = reinterpret_cast<char*>(malloc(tableBufferSize));
-    do {
-        if (tableBuffer == nullptr) {
-            break;
-        }
-        if (GetSystemFirmwareTable(FirmwareTableProviderSignature,
-                                   HPETSignature, tableBuffer,
-                                   tableBufferSize) == 0) {
-            break;
-        }
-        const auto bodyAddress =
-            reinterpret_cast<char*>(reinterpret_cast<uint64_t>(tableBuffer) +
-                                    sizeof(acpi_table_header));
-        memcpy(&hpetData, bodyAddress, sizeof(hpet_acpi_data));
-        isSuccess = true;
-    } while (false);
-    if (tableBuffer) {
-        free(tableBuffer);
-    }
-    return {isSuccess, std::make_shared<hpet_acpi_data>(hpetData)};
-}
 auto main() -> int {
     printf("acpi sandbox detect by huoji 2023.6.19 \n");
-    /*
+
     auto firmwareTableBufferSize =
         EnumSystemFirmwareTables(FirmwareTableProviderSignature, 0, 0);
     char* firmwareTableBuffer = nullptr;
@@ -77,6 +46,7 @@ auto main() -> int {
         if (firmwareTableBufferSize == 0) {
             break;
         }
+        bool foundHpet = false;
         auto index = 0;
         for (size_t i = 0; i < firmwareTableBufferSize / 4; i++) {
             const auto tableHeader = reinterpret_cast<acpi_table_header*>(
@@ -88,32 +58,35 @@ auto main() -> int {
             memcpy(oemid, reinterpret_cast<char*>(tableHeader->oem_id),
                    sizeof(tableHeader->oem_id));
 
-            printf("tid: %s oemid: %s oem_table_id: %08X \n", tid, oemid,
-                   tableHeader->oem_table_id);
+            // printf("tid: %s oemid: %s oem_table_id: %llX oem_table_xd: %llX
+            // \n",
+            //        tid, oemid, tableHeader->oem_table_id,
+            //        (tableHeader->oem_table_id & 0xFFFFFFFF));
             if (memcmp(oemid, "WAET", 4) == 0) {
                 printf(
                     "[detected] Vmware detected by Windows ACPI Emulated"
                     "\n");
             }
-            if (tableHeader->oem_table_id == 0) {
+            if (foundHpet == false && memcmp(oemid, "HPET", 4) == 0) {
+                foundHpet = true;
+            }
+            if ((tableHeader->oem_table_id & 0xFFFFFFFF) == 0) {
                 printf(
                     "[detected] Cuckoo sandbox detected by oem table id"
                     "\n");
             }
             index++;
         }
-        if (index < 7) {
-            printf("[detected] table size detected \n");
+        if (index < 8) {
+            printf("[detected] vm-guest detected by table size \n");
+        }
+        if (foundHpet == false) {
+            printf("[detected] HPET not found,Cuckoo detected \n");
         }
 
     } while (false);
     if (firmwareTableBuffer != nullptr) {
         free(firmwareTableBuffer);
-    }
-    */
-    auto [isSuccess, data] = getHPETData();
-    if (isSuccess) {
-        __debugbreak();
     }
     getchar();
     return 0;
